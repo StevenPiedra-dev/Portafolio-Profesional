7c9fb84e70e729aa6c89d134568a0868fad1d6af
Steven Antonio Piedra Villalta <steven.piedra02@gmail.com>
2025-10-28 20:02:28 -0600

    Update About me
    Se trabaja en el about me de user

---
+++ pages/footer/footer.js
@@ -0,0 +1,10 @@
+// js/footer.js
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Cargando footer desde:", window.location.href);
+    fetch('../../footer/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        })
+    .catch(error => console.error("Error loading footer:", error));
+});
