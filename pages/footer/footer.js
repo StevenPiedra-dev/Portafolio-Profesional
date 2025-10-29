4764585f4436f80f6b1c627bc0d685e6f5b5dc63
Steven Antonio Piedra Villalta <steven.piedra02@gmail.com>
2025-10-28 20:39:29 -0600

    Rutas
    Rutas

--- pages/footer/footer.js
+++ pages/footer/footer.js
@@ -1,7 +1,7 @@
 // js/footer.js
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Cargando footer desde:", window.location.href);
-    fetch('pages/footer/footer.html')
+    fetch('../../pages/footer/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
