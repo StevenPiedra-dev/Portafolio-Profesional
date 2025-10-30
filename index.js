403166845eff4e5d55e1d4b1a5b2c5fb54b7b147
Steven Antonio Piedra Villalta <steven.piedra02@gmail.com>
2025-10-29 22:08:42 -0600

    Header a todas las paginas

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-  const menuToggle = document.getElementById("menuToggle");
-  const navLinks = document.querySelector(".nav-links");
+/* Busca en el HTML con el atributo menuToogle y busca el elemnto que cntiene todos los links de la lista */
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.querySelector(".nav-links");
 
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
+/*Escucho cuando el usuario hace clic en el icono del menù y agrego o quito el "active"*/
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
-  // 👇 Esto cierra el menú cuando haces clic en un enlace
-  document.querySelectorAll(".nav-links a").forEach(link => {
-    link.addEventListener("click", () => {
-      navLinks.classList.remove("active");
-    });
+// 👇 Esto cierra el menú cuando haces clic en un enlace
+document.querySelectorAll(".nav-links a").forEach(link => { /** Obtengo todos los enlaces dentro del menu y con el foreach recorro cada uno de llos*/
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active"); /*Si hay un click, cierra el menú*/
   });
+});
