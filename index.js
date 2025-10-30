6a05521f60362438626af8e2595dbd51184ffd97
Steven Antonio Piedra Villalta <steven.piedra02@gmail.com>
2025-10-29 18:38:43 -0600

    Se implementa la navbar
    Se implementa la nav bar en el index usuario, la misma queda 100% funcional, responsive y ya el botón redirige al login para administrador

---
+++ index.js
@@ -0,0 +1,13 @@
+  const menuToggle = document.getElementById("menuToggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+  // 👇 Esto cierra el menú cuando haces clic en un enlace
+  document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+    });
+  });
